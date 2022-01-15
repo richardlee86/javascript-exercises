50773ac7d220f0b9b8a4e81baeec810d68adfd56
Richard Lee <57221618+richardlee86@users.noreply.github.com>
2022-01-15 21:07:01 +0100

    completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
 const reverseString = function(str) {
+    this.str = str;
+    let arr = []; //array to hold original string    
+    let reversedArr = [] //array to hold joined reversed string
+    
 
+    for (let i = 0; i < str.length; i++) {       
+        let arrVariable = str.charAt(i);
+        arr.unshift(arrVariable);
+    }
+    reversedArr = arr.join('');
+    return reversedArr; 
 };
 
 // Do not edit below this line
