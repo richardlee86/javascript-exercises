0fa727c9fd942940cc1d262868a7d2fdfc6b9fc8
Richard Lee <57221618+richardlee86@users.noreply.github.com>
2022-01-15 20:01:54 +0100

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    this.string = string; 
+    this.num = num;
+    let arr = [];
+    let finalArr = [];    
+    if(num < 0){
+        return 'ERROR';
+    } else if (string === ''){
+        return '';
+    }
+    for (let i = 0; i < num; i++) {                
+        arr.push(string);                   
+    }
+    finalArr = arr.join("");
+    return finalArr.toString();
 };
 
 // Do not edit below this line
