35096301c970827e3aadc2e3467a83ab7789acdd
Richard Lee <57221618+richardlee86@users.noreply.github.com>
2022-01-17 17:07:33 +0100

    completed exercise 8 to specifications

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2; 
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2; 
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const numAdder = (firstNum, currentNum) => firstNum + currentNum;
+  
+  if (array.length > 1){
+    return array.reduce(numAdder);
+  } else if (array.length === 1) {
+    return array[0];
+  }	else {
+    return 0;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const numMultiplyer = (firstNum, currentNum) => firstNum * currentNum;
+  return array.reduce(numMultiplyer);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return num ** power; 
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num <= 0){
+    return 1;
+  } else if (num === 1) {
+    return 1; 
+  } else {
+    return (num * factorial(num - 1));
+  }
 };
 
 // Do not edit below this line
