930b85f0ce116400b31db6180b3a0408f8d433a2
Richard Lee <57221618+richardlee86@users.noreply.github.com>
2022-01-17 21:22:39 +0100

    completed exercise 10 to specifications

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = (num) => {
+    let arr = [1, 1]; //array to hold fibonacci sequence
+    let n = 1, n2 = n, nextRound; 
 
+    //Don't allow negative numbers as parameters
+    if (num < 0) {
+        return 'OOPS';
+    }
+    //loop to populate an array with the fibonacci sequence
+    for (let i = 1; i < 50; i++) {        
+         nextRound = n + n2;
+         n = n2;
+         n2 = nextRound;
+         arr.push(nextRound);
+    }    
+    //return specific position in the sequence    
+    return arr[num - 1];
 };
 
 // Do not edit below this line
