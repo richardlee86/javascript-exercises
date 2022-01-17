44c32fd6b5be3048430da72f89ba1cfb70b191c7
Richard Lee <57221618+richardlee86@users.noreply.github.com>
2022-01-17 13:29:02 +0100

    completed exercise 5 to specifications

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let finalSum; //create a variable to hold final sum
+    let arr = []; //create array to push numbers to
+    let count = num1;
+    const numAdder = (firstNum, currentNum) => firstNum + currentNum;
+
+    if(num1 < num2 && num1 > 0 && typeof num1 === 'number'
+    && typeof num2 === 'number'){
+        while (count <= num2) {
+            arr.push(count);
+            count++;
+        }
+    } else if (num1 > num2 && num1 > 0 && typeof num1 === 'number'
+    && typeof num2 === 'number') {
+        while (count >= num2) {
+            arr.push(count);
+            count--;
+        }
+    } else if(num1 < 0 || num2 < 0){
+        return 'ERROR';
+    } else if (typeof num1 !== 'number' || typeof num2 !== 'number'){
+        return 'ERROR';
+    }
+    
+    return arr.reduce(numAdder);
 
 };
 
