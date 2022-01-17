f356be5dad6829527ae954608dc89030df31f758
Richard Lee <57221618+richardlee86@users.noreply.github.com>
2022-01-17 12:37:52 +0100

    completed exercise 4 to specifications

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr=[], ...args) {
+    this.arr = arr; 
+    this.args = args;      
+    let newArgs = Array.from(args); //array to hold unknown parameters
 
+    /*create a new array to hold the remaining elements after removing the
+    elements specified by the arguments*/
+    const newArray = arr.filter(function(i){        
+        return !newArgs.includes(i);
+    });
+    
+    //return the new array that doesn't include those removed
+    return newArray;    
 };
 
 // Do not edit below this line
