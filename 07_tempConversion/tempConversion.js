d32ea5b33318ba41591479c3240878455a54dc68
Richard Lee <57221618+richardlee86@users.noreply.github.com>
2022-01-17 16:30:09 +0100

    completed exercise 7 to specifications

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  return Math.ceil((((num - 32) * 5) / 9 ) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  return Math.ceil((num * (9/5) + 32) * 10) / 10;
 };
+
+
 
 // Do not edit below this line
 module.exports = {
